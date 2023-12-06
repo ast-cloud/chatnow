a5f8d0ec54c3608471d00585d633caab12f18e16
Aastik Yadav <aastikyadav1997.ay@gmail.com>
2023-12-06 20:03:13 +0530

    Frontend initial commit

--- backend/index.ts
+++ backend/index.ts
@@ -1,9 +1,22 @@
 import express from 'express';
+import http from 'http';
+import {WebSocketServer} from 'ws';
 
 const app = express();
 
+const server = http.createServer(app);
+
+const wss = new WebSocketServer({server});
+
 app.get('/healthcheck', (req, res)=>{
     res.json({'status':'healthy'});
+});
+
+wss.on('connection', async (ws, req)=>{
+    ws.on('message', (message)=>{
+        console.log('Received message - ', String(message));
+        ws.send('You have sent - '+message);
+    })    
 })
 
-app.listen(3000, ()=>{console.log('Started express server on port 3000.')})
+server.listen(3000, ()=>{console.log('Started http server on port 3000.')})
---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div style={{display:'flex'}}>
+      <button>Create a room</button>
+      &nbsp;&nbsp;&nbsp;&nbsp;
+      <button>Join a room</button>
+    </div>
+  )
+}
+
+export default App
