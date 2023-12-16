c4e9afc5eff3250013d45efa7f8ffaa81d8d4e42
Aastik Yadav <aastikyadav1997.ay@gmail.com>
2023-12-17 03:11:41 +0530

    Added chat text field and send button

--- frontend/src/components/ChatPage.tsx
+++ frontend/src/components/ChatPage.tsx
@@ -1,10 +1,26 @@
 import {useState} from 'react';
+import { Button, Textarea } from '@material-tailwind/react';
+
+type message = {
+    name: string;
+    text: string;
+}
 
 export default function ChatPage(){
+    
+    const [messages, setMessages] = useState<message[]|null>(null);
 
-    return <div>
-        <div className='w-full h-10 bg-gray-500'>
+    return <div className='flex flex-col justify-between h-screen'>
+        <div className='w-full h-20 bg-[#F5F3F3]'>
             ChatPage
+        </div>
+        <div className='flex-grow'>
+
+        </div>
+        <div className='w-full h-21 bg-[#F5F3F3] flex flex-row p-5 flex-g'>
+            <Textarea placeholder='Type a message' className='w-12/14 bg-white'/>
+            <div className='w-2'/>
+            <Button placeholder='' className='w-2/14 h-10 place-self-center'>Send</Button>
         </div>
     </div>
 }
