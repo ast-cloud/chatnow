580bbe736657dec031bf8265f9126ab2a64564e6
Aastik Yadav <aastikyadav1997.ay@gmail.com>
2023-12-17 03:09:16 +0530

    Added onmessage handler to websocket

--- frontend/src/lib/ws.ts
+++ frontend/src/lib/ws.ts
@@ -1,17 +1,47 @@
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 export default class WSManager{
     
     public static instance: WSManager;
+    private websocket: WebSocket;
     
-    private constructor(){
-        const websocket = new WebSocket('ws://localhost:3000');
+    private constructor(setNavigateToUrl){
+        
+
+        this.websocket = new WebSocket('ws://localhost:3000');
+
+        this.websocket.onmessage = (event)=>{
+            const data = JSON.parse(event.data);
+            if(data.type=='roomCreated'){
+                toast.success('Room ID : '+ String(data.payload.roomId));
+                console.log('About to call navigate("/chat")');
+                setNavigateToUrl('/chat');
+            }
+            else if(data.type=='roomJoined'){
+                toast.success('Room ID : '+ String(data.payload.roomId));
+                setNavigateToUrl('/chat');
+            }
+            else if(data.type=='roomCreationFailed'){
+                toast.error(data.payload.message);
+            }
+            else if(data.type=='roomJoinFailed'){
+                toast.error(data.payload.message);
+            }
+        }
+
     }
 
-    static getInstance(){
+    static getInstance(setNavigateToUrl?){
         if(!this.instance){
-            this.instance = new WSManager();
+            this.instance = new WSManager(setNavigateToUrl);
         }
         return this.instance;
+    }
+
+    sendData(data: string){
+        this.websocket.send(data);
     }
 
 }
