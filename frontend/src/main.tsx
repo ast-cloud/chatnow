d7ecd1bceae515c8391c060f49b0d0c520e3e329
Aastik Yadav <aastikyadav1997.ay@gmail.com>
2023-12-18 11:54:12 +0530

    Wrapped App component in RecoilRoot

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { ThemeProvider } from "@material-tailwind/react";
+import {RecoilRoot} from 'recoil';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider>
-      <App />
+      <RecoilRoot>
+        <App />
+      </RecoilRoot>
     </ThemeProvider>
   </React.StrictMode>,
 )
