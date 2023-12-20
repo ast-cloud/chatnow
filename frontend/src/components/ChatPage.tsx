c997990e82fe6449a57ab37ca90e1ebd0cdaffc4
Aastik Yadav <aastikyadav1997.ay@gmail.com>
2023-12-20 18:58:21 +0530

    Fixed chat textarea UI problem.

--- frontend/src/components/ChatPage.tsx
+++ frontend/src/components/ChatPage.tsx
@@ -30,7 +30,8 @@ export default function ChatPage(){
 
     useEffect(()=>{
         return ()=>{
-            WSManager.getInstance().closeConnection();            
+            console.log('Closing ws connection.')
+            //WSManager.getInstance().closeConnection();            
         }
     },[]);
     
@@ -51,22 +52,31 @@ export default function ChatPage(){
     }
 
     return <div className='flex flex-col justify-between h-screen'>
-        <div className='w-full h-20 bg-[#F5F3F3]'>
-            Room name : {roomName} &nbsp; Room ID : {roomId}
+
+        <div className='w-full h-20 px-5 bg-[#F5F3F3] font-bold flex flex-row justify-between items-center' style={{color:'#414142'}}>
+            <Typography placeholder='' variant='h5'>
+                {roomName} 
+            </Typography>
+            <Typography placeholder='' variant='h5'>
+                (Room ID : {roomId})
+            </Typography>
         </div>
+
         <div id='chatDiv' className='flex-grow overflow-y-auto'>
             {messages.map((m, index)=>(
-                <div key={`message_${index}`} className='flex flex-col bg-[#f5f3f3] m-4 p-1 pl-2 max-w-[50vw] rounded-md'>
-                    <p style={{fontSize:'14px'}}>{m.name}</p>                                        
-                    <Typography placeholder='' variant='lead'>{m.message}</Typography>                                        
+                <div key={`message_${index}`} className='flex flex-col bg-[#f5f3f3] m-4 p-2 pl-4 max-w-[50vw] rounded-lg'>
+                    <p style={{fontSize:'14px', fontWeight:'bold'}}>{m.name}</p>                                        
+                    <p style={{fontSize:'16px'}}>{m.message}</p>                          
                 </div>
             ))}
         </div>
-        <div className='w-full h-21 bg-[#F5F3F3] flex flex-row p-5 flex-g'>
-            <Textarea placeholder='Type a message' className='w-12/14 bg-white' value={newMessage} onChange={handleChange}/>
+
+        <div className='w-full h-25 bg-[#F5F3F3] flex flex-row p-5  '>
+            <textarea id="chat" rows={1} className="block mx-4 p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Type a message" value={newMessage} onChange={handleChange}></textarea>
             <div className='w-2'/>
             <Button placeholder='' className='w-2/14 h-10 place-self-center' onClick={handleSend}>Send</Button>
         </div>
+
         <ToastContainer/>
     </div>
 }
