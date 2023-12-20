3d6656a4b9a2744541709c82369d9932b1594716
Aastik Yadav <aastikyadav1997.ay@gmail.com>
2023-12-20 19:56:06 +0530

    Added external css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Landing from './components/Landing';
 import ChatPage from './components/ChatPage';
+import './App.css';
 
 function App() {
 
