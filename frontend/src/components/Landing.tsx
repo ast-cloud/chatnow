f55df9d6f4c4dc2b9788ef11782bdb7c221bd4a5
Aastik Yadav <aastikyadav1997.ay@gmail.com>
2023-12-25 16:15:34 +0530

    Added animation on home page

--- frontend/src/components/Landing.tsx
+++ frontend/src/components/Landing.tsx
@@ -4,6 +4,8 @@ import { useNavigate } from "react-router-dom";
 import WSManager from "../lib/ws";
 import {useSetRecoilState} from 'recoil';
 import { chatMessages } from '../lib/atoms/chatPage';
+import Lottie from 'react-lottie-player';
+import homePageAnimation from '../../public/home_page_animation.json';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -18,6 +20,18 @@ export default function Landing(){
     const [dialogOpen, setDialogOpen] = useState<boolean>(false);
     const [createOrJoin, setCreateOrJoin] = useState('create');
 
+    const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+          setIsSmallScreen(window.innerWidth < 600);
+        };
+        window.addEventListener('resize', handleResize);
+        handleResize();
+    
+        return () => window.removeEventListener('resize', handleResize);
+      }, []);
+
     function createRoom(){
         setCreateOrJoin('create');
         setDialogOpen(!dialogOpen);
@@ -29,11 +43,68 @@ export default function Landing(){
     }
 
     return (
-        <div style={{display:'flex', justifyContent:'space-evenly', alignItems:'center', height:'100vh', width:'100vw', border:'1px solid black'}}>
-            <Button placeholder='' size="lg" onClick={createRoom}>Create a room</Button>
-            {/* <div style={{width:'100px'}}></div> */}
-            <Button placeholder='' size="lg" onClick={joinRoom}>Join a room</Button>
-            <DialogWithForm dialogOpen={dialogOpen} setDialogOpen={setDialogOpen} createOrJoin={createOrJoin}/>
+        <div style={{display:'flex', flexDirection:isSmallScreen?'column':'row', height:'100vh'}}>
+
+            <div style={{display:'flex', flexDirection:'column', width:'50vw', backgroundColor:''}}>
+
+                <div style={{display:'flex', alignItems:'center', height:'35vh', paddingLeft:'15%', border:'0px solid black'}}>
+                    <div className="floating-animation" style={{height:'100px', width:'100px'}}>
+                        <img src="/chatnow_logo.png" className="floating-animation-item" alt=""/>
+                    </div>
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <h1 style={{fontSize:'50px', fontFamily:"cursive", fontWeight:'bold', color: '#05e3c5'}}>Chatnow</h1>
+                </div>
+
+                <div style={{display:'flex', flexDirection:'column', height:'25vh', justifyContent:'center', paddingLeft:'20%', border:'0px solid green'}}>
+                    
+                        <p style={{fontSize:'20px', fontFamily:'cursive', fontWeight:'bold'}}>Quick, anonymous chatting application</p>
+                        <p style={{fontSize:'20px', fontFamily:'cursive', fontWeight:'bold'}}>Hidden identity</p>
+                        <p style={{fontSize:'20px', fontFamily:'cursive', fontWeight:'bold'}}>Encrypted</p>
+                        <p style={{fontSize:'20px', fontFamily:'cursive', fontWeight:'bold'}}>No logs</p>
+
+                    
+                </div>
+
+                
+                <div style={{display:'flex', height:'40vh', justifyContent:'space-around', paddingTop:'15%', border:'0px solid yellow'}}>
+                    <Button placeholder='' size="lg" style={{height:'45px', borderRadius:'40px'}} onClick={createRoom}>Create a room</Button>
+                    <Button placeholder='' size="lg" style={{height:'45px', borderRadius:'40px'}} onClick={joinRoom}>Join a room</Button>
+                    <DialogWithForm dialogOpen={dialogOpen} setDialogOpen={setDialogOpen} createOrJoin={createOrJoin}/>
+                </div>
+                
+
+                
+            </div>
+
+            <div style={{display:'flex', justifyContent:'center', alignItems:'center', width:'50vw', backgroundColor:''}}>
+                <Lottie loop animationData={homePageAnimation} play speed={2.0} style={{height:450, width:450}}/>
+            </div>
+
+            {/* <div style={{display:'flex', flexDirection: isSmallScreen?'column':'row'}}>
+                <div style={{display:'flex', height:'40vh', width:'50vw', border:'0px solid black'}}>
+                    <div style={{height:'100px', width:'100px', marginTop:'7vh', marginLeft:'25vh'}}>
+                        <img src="/chatnow_logo.png" alt=""/>
+                    </div>
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <h1 style={{fontSize:'50px', fontFamily:"cursive", fontWeight:'bold', color: '#05e3c5', marginTop:'7vh'}}>Chatnow</h1>
+                </div>
+                <div style={{display:'flex', width:'50vw', paddingLeft:'55px', paddingTop:'85px', alignItems:'center', border:'0px solid red'}}>
+                    <div>
+                        <p style={{fontSize:'20px', fontFamily:'cursive', fontWeight:'bold'}}>Quick, anonymous chatting application</p>
+                        <p style={{fontSize:'20px', fontFamily:'cursive', fontWeight:'bold'}}>Hidden identity</p>
+                        <p style={{fontSize:'20px', fontFamily:'cursive', fontWeight:'bold'}}>Encrypted</p>
+                        <p style={{fontSize:'20px', fontFamily:'cursive', fontWeight:'bold'}}>No logs</p>
+
+                    </div>
+                </div>
+            </div>
+            <div style={{display:'flex'}}>
+                <div style={{display:'flex', justifyContent:'space-evenly', alignItems:'center', height:'60vh', width:'50vw', border:'0px solid black'}}>
+                    <Button placeholder='' size="lg" onClick={createRoom}>Create a room</Button>
+                    <Button placeholder='' size="lg" onClick={joinRoom}>Join a room</Button>
+                    <DialogWithForm dialogOpen={dialogOpen} setDialogOpen={setDialogOpen} createOrJoin={createOrJoin}/>
+                </div>
+            </div> */}
         </div>
     );
 }
